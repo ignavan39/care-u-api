067c616119e4c0c986b8d3c3d43486764a6d8de4
ignavan39 <ivanignatenko28@gmail.com>
2021-12-12 20:06:21 +0600

    chore: swagger added

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  const config = new DocumentBuilder()
+    .setTitle('Care U API')
+    .setVersion('1.0')
+    .addTag('careU')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
